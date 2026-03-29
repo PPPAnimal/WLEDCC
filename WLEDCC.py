0a8a9fc598600741c1f96364c48d9ab44fe619ea
PPPAnimal <privatebillsullivan@gmail.com>
2026-03-28 22:16:17 -0400

    Add License

--- WLEDCC.py
+++ WLEDCC.py
@@ -1,3 +1,25 @@
+""" WLEDCC - WLED COMMAND CENTER
+    Copyright (C) 2026 Bill Sullivan (SullySSignS)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>. """
+
+# If you find this software useful, consider supporting the developer.
+# [Buy Me a Coffee] https://paypal.me/BillSullivan02
+# Inquiries: privatebillsullivan@gmail.com
+# 
+# 
+# 
 # ledfx_monitor_loop
 # fetch_latest_release
 # check_ledfx_updates
